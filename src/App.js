93329328c48d1513dc693513b48032a92dfb8587
Jeff Appareti <jappareti@gmail.com>
2018-10-17 11:35:57 -0700

    Installed netlify-lambda with Hello World example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, msg: null };
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    fetch("/.netlify/functions/hello")
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }));
+  };
+
+  render() {
+    const { loading, msg } = this.state;
+
+    return (
+      <p>
+        <button onClick={this.handleClick}>
+          {loading ? "Loading..." : "Call Lambda"}
+        </button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -11,14 +41,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <LambdaDemo />
         </header>
       </div>
     );
