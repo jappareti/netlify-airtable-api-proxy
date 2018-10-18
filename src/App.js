a2698b804b200cc7d2ba85b2bd2937714f0da718
Jeff Appareti <jappareti@gmail.com>
2018-10-18 09:28:27 -0700

    Added pagination

--- src/App.js
+++ src/App.js
@@ -9,40 +9,55 @@ import Gallery from "./components/Gallery";
 class App extends Component {
   state = {
     records: [],
-    loading: false,
+    loading: true,
+    pageSize: 6,
+    offset: "",
     error: null
   };
 
   componentDidMount() {
-    this.setState({ loading: true });
+    this.getRecords();
+  }
+
+  getRecords = () => {
+    this.setState({loading: true})
     axios
       .get("/.netlify/functions/api-proxy", {
         params: {
           url: `https://api.airtable.com/v0/${
             process.env.REACT_APP_AIRTABLE_BASE_ID
-          }/Artists`
+          }/Artists?pageSize=${this.state.pageSize}&offset=${this.state.offset}`
         }
       })
-      .then(response =>
-        this.setState({ loading: false, records: response.data.records })
+      .then(
+        response =>
+          console.log(response) ||
+          this.setState(state => {
+            const records = state.records.concat(response.data.records);
+            return {
+              loading: false,
+              records,
+              offset: response.data.offset || "stop"
+            };
+          })
       )
       .catch(error => {
         this.setState({ loading: false, error });
       });
-  }
+  };
 
   toggleOnDisplay = id => e => {
     const target = e.target;
 
     // Update the state first
-    this.setState((state => {
+    this.setState(state => {
       const record = state.records.filter(record => record.id === id)[0];
-      record.fields['On Display?'] = target.checked;
+      record.fields["On Display?"] = target.checked;
       return {
         ...state.records,
         record
-      }
-    }))
+      };
+    });
 
     // Update via Airtable API proxy
     // If it fails, show the error page
@@ -65,19 +80,24 @@ class App extends Component {
   };
 
   render() {
-    const { records, loading, error } = this.state;
+    const { records, loading, error, offset } = this.state;
 
     if (error !== null) {
       return <Error error={error} />;
     }
 
-    if (loading) {
+    if (loading && records.length === 0) {
       return <LoadingIndicator />;
     }
 
     return (
       <div className="App">
         <Gallery records={records} toggleOnDisplay={this.toggleOnDisplay} />
+        {offset !== "stop" && (
+          <button onClick={this.getRecords} className="btn-more">
+            {loading ? "Loading..." : "More..."}
+          </button>
+        )}
       </div>
     );
   }
